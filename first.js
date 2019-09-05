f26ecd32be77069f1b802ec9234b533e0e0f6932
aakashDeveloper <ahanda205@gmail.com>
2019-09-05 04:16:19 +0100

    a

---
+++ first.js
@@ -0,0 +1,127 @@
+string
+number
+boolean
+
+Es5
+
+var city = "london";
+var city = 'london';
+var marks = "10";
+var option = "true";
+var test = 'Maths"
+
+var marks = 8687;
+var speed = 10.98;
+
+var option = true
+var negative = false
+
+
+var city = "london"
+undefined
+typeof(city)
+"string"
+var city = "10"
+undefined
+typeof(city)
+"string"
+var city = 10
+undefined
+typeof(city)
+"number"
+var city = true
+undefined
+typeof(city)
+"boolean"
+
+Anything between quotes is string
+Number or decimal is Number
+true/false  is boolean
+
+
+/****************
+ *Operations
+ *************/
+
+ var a = 10
+ var b = 10
+
+ var a = 10
+ var b = 10
+undefined
+a+b
+20
+a-b
+0
+a*b
+100
+a/b
+1
+var c = 10.18
+undefined
+a+c
+20.18
+a+b+c
+30.18
+
+
++ with number addition 
++ with string concat
+NaN
+Not a number
+
+var a = "Hi"
+undefined
+var b = " JavaScript"
+undefined
+a+b
+"Hi JavaScript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+
+1+" hi"
+"1 hi"
+"hi "+1
+"hi 1"
+
+number + string = string
+number + number = number
+string + number = string
+string + string = string
+
+10+"10"+10
+"101010"
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+
+
+true  is equal to 1
+false is equal to 0
+
+true+true 
+false +false 
+
+truthy/false
+
+truthy
+>=1, true
+
+falsely
+0, -1, false, null, undefined
+
+true+true
+2
+false+false
+0
+5+true
+6
+"true"+"true"
+true
+ number+boolean ="number"
+ boolean+string = "string"
---
+++ second.js
@@ -0,0 +1,41 @@
+var a =10 => assignment
+var a == 10 => compare data only
+var a === 10 => compare data and data type
+
+var a = 10
+undefined
+a
+10
+var b = "10"
+undefined
+b
+"10"
+a == b
+true
+a === b
+false
+
+
+console.log("hiii")
+
+console.log("hiii")
+VM1341:1 hiii
+undefined
+alert("hiii")
+undefined
+prompt('What is your name')
+"jjjj"
+var name = prompt('What is your name')
+undefined
+name
+"aakash"
+confirm('Do you want to leave')
+true
+confirm('Do you want to leave')
+false
+var name = prompt('What is your name')
+undefined
+var name = prompt('What is your name')
+undefined
+name
+""
