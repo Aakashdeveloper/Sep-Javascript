141a9f04595e29b9a45cd57c4997d9c76af15ffb
aakashDeveloper <ahanda205@gmail.com>
2019-09-06 04:17:12 +0100

    a

---
+++ four.js
@@ -0,0 +1,90 @@
+10 + 10 = 20
+"10"+"10" = "1010"
+
+var a = "10"
+var b = '20'
+
+parseInt(a)+parseInt(b)
+var a = "10"
+var b = '20'
+
+parseInt(a)+parseInt(b)
+30
+a+b
+"1020"
+var c = "ab"
+undefined
+parseInt(c)
+NaN
+
+var x = "10.1234"
+var y = "10"
+parseInt(x)+parseInt(y)
+
+
+var x = "10.1234"
+var y = "10"
+x<y
+
+
+var a = 10
+var b = "10"
+
+var x = "10.1234"
+var y = "10"
+parseInt(x)+parseInt(y)
+20
+parseFloat(x)+parseFloat(y)
+20.1234
+var x = "10.1234"
+var y = "10"
+x<y
+false
+x>y
+true
+x == y
+false
+x !== y
+true
+x != y
+true
+var a = 10
+var b = "10"
+undefined
+a<b
+false
+a>b
+false
+a == b
+true
+a !=b
+false
+
+var a = 10
+var b = 20
+
+a == 10 || b == 20
+a == 10 && b == 20
+
+a == 10 || b == 21  => true
+a == 10 && b == 21  => false
+
+true && true
+true && false
+
+
+0&&1 = 0
+
+
+var a = true
+undefined
+var b= true
+undefined
+b !== !a
+true
+
+
+a = true
+b = true
+
+true  !== false
---
+++ third.js
@@ -0,0 +1,125 @@
+var city = "london"
+var city = "london"
+undefined
+city.toUpperCase()
+"LONDON"
+var lang = "JAvASCript"
+undefined
+lang.toLowerCase()
+"javascript"
+
+var city = "london"
+undefined
+city.toUpperCase()
+"LONDON"
+var lang = "JAvASCript"
+undefined
+lang.toLowerCase()
+"javascript"
+city.length
+6
+var a = " i am doing javascript"
+undefined
+a.replace('javascript', 'react')
+" i am doing react"
+a.repeat(2)
+" i am doing javascript i am doing javascript"
+var name = "   Aakash "
+undefined
+name.length
+10
+name.trim()
+"Aakash"
+var final = name.trim()
+undefined
+final.length
+6
+final.charAt(0)
+"A"
+final.charAt(4)
+"s"
+final.slice(1)
+"akash"
+final.slice(2)
+"kash"
+final.slice(2,4)
+"ka"
+
+var city = "london"
+undefined
+city.toUpperCase()
+"LONDON"
+var lang = "JAvASCript"
+undefined
+lang.toLowerCase()
+"javascript"
+city.length
+6
+var a = " i am doing javascript"
+undefined
+a.replace('javascript', 'react')
+" i am doing react"
+a.repeat(2)
+" i am doing javascript i am doing javascript"
+var name = "   Aakash "
+undefined
+name.length
+10
+name.trim()
+"Aakash"
+var final = name.trim()
+undefined
+final.length
+6
+final.charAt(0)
+"A"
+final.charAt(4)
+"s"
+final.slice(1)
+"akash"
+final.slice(2)
+"kash"
+final.slice(2,4)
+"ka"
+
+
+var name= "aMsterdAM"
+undefined
+name.toUpperCase().charAt(0)
+"A"
+name.slice(1)
+"MsterdAM"
+name.slice(1).toLowerCase()
+"msterdam"
+name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()
+"Amsterdam"
+name
+"aMsterdAM"
+var final  = name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()
+undefined
+final
+"Amsterdam"
+final.length()
+VM1356:1 Uncaught TypeError: final.length is not a function
+    at <anonymous>:1:7
+(anonymous) @ VM1356:1
+final.length
+9
+final.length()
+
+var url = "https://stackoverflow.com/questions/25045946/where-should-my-npm-modules-be-installed-on-mac-os-x"
+undefined
+url.split('/')
+(6) ["https:", "", "stackoverflow.com", "questions", "25045946", "where-should-my-npm-modules-be-installed-on-mac-os-x"]0: "https:"1: ""2: "stackoverflow.com"3: "questions"4: "25045946"5: "where-should-my-npm-modules-be-installed-on-mac-os-x"length: 6__proto__: Array(0)
+url.split('/')[4]
+"25045946"
+var mytext =  "i am happy"
+undefined
+mytext.split(" ")
+(3) ["i", "am", "happy"]
+var name="aakash"
+undefined
+name.split('')
+(6) ["a", "a", "k", "a", "s", "h"]
+
+
